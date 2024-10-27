74a9e1b5b481cdce98bbe4c9b85a71903fd8d91d
A6575 <astridaalvaradoa@gmail.com>
2024-10-27 18:35:51 -0400

    Agregado archivo src/messaging/unread.js generado por la traducción de TS a JS del proyecto 1

--- src/messaging/unread.js
+++ src/messaging/unread.js
@@ -1,18 +1,35 @@
 'use strict';
 
+const __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+	function adopt(value) { return value instanceof P ? value : new P((resolve) => { resolve(value); }); }
+	P = P || Promise;
+	return new P((resolve, reject) => {
+		function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+		function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+		function step(result) {
+			if (result.done) {
+				resolve(result.value);
+			} else {
+				adopt(result.value).then(fulfilled, rejected);
+			}
+		}
+		step((generator = generator.apply(thisArg, _arguments || [])).next());
+	});
+};
+/* eslint-disable import/no-import-module-exports */
 const db = require('../database');
 const io = require('../socket.io');
-
+// Codigo principal
 module.exports = function (Messaging) {
-	Messaging.getUnreadCount = async (uid) => {
+	Messaging.getUnreadCount = uid => __awaiter(this, undefined, undefined, function* () {
 		if (!(parseInt(uid, 10) > 0)) {
 			return 0;
 		}
-
-		return await db.sortedSetCard(`uid:${uid}:chat:rooms:unread`);
-	};
-
-	Messaging.pushUnreadCount = async (uids, data = null) => {
+		// eslint-disable-next-line max-len
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
+		return yield db.sortedSetCard(`uid:${uid}:chat:rooms:unread`);
+	});
+	Messaging.pushUnreadCount = (uids, data = null) => {
 		if (!Array.isArray(uids)) {
 			uids = [uids];
 		}
@@ -20,55 +37,47 @@ module.exports = function (Messaging) {
 		if (!uids.length) {
 			return;
 		}
-		uids.forEach((uid) => {
-			io.in(`uid_${uid}`).emit('event:unread.updateChatCount', data);
-		});
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+		uids.forEach((uid) => { io.in(`uid_${uid}`).emit('event:unread.updateChatCount', data); });
 	};
-
-	Messaging.markRead = async (uid, roomId) => {
-		await Promise.all([
-			db.sortedSetRemove(`uid:${uid}:chat:rooms:unread`, roomId),
-			db.setObjectField(`uid:${uid}:chat:rooms:read`, roomId, Date.now()),
+	Messaging.markRead = (uid, roomId) => __awaiter(this, undefined, undefined, function* () {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+		yield Promise.all([db.sortedSetRemove(`uid:${uid}:chat:rooms:unread`, roomId), db.setObjectField(`uid:${uid}:chat:rooms:read`, roomId, Date.now()),
 		]);
-	};
-
-	Messaging.hasRead = async (uids, roomId) => {
+	});
+	Messaging.hasRead = (uids, roomId) => __awaiter(this, undefined, undefined, function* () {
 		if (!uids.length) {
 			return [];
 		}
-		const roomData = await Messaging.getRoomData(roomId);
+		const roomData = yield Messaging.getRoomData(roomId);
 		if (!roomData) {
 			return uids.map(() => false);
 		}
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 		if (roomData.public) {
-			const [userTimestamps, mids] = await Promise.all([
-				db.getObjectsFields(uids.map(uid => `uid:${uid}:chat:rooms:read`), [roomId]),
-				db.getSortedSetRevRangeWithScores(`chat:room:${roomId}:mids`, 0, 0),
-			]);
-			const lastMsgTimestamp = mids[0] ? mids[0].score : 0;
-			return uids.map(
-				(uid, index) => !userTimestamps[index] ||
-					!userTimestamps[index][roomId] ||
-					parseInt(userTimestamps[index][roomId], 10) > lastMsgTimestamp
-			);
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+			const [userTimestamps, mids] = yield Promise.all([db.getObjectsFields(uids.map(uid => `uid:${uid}:chat:rooms:read`), [roomId]), db.getSortedSetRevRangeWithScores(`chat:room:${roomId}:mids`, 0, 0)]);
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+			const lastMsgTimestamp = (mids.length > 0 && mids[0].score) ? mids[0].score : 0;
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+			return uids.map((uid, index) => !userTimestamps[index] || !userTimestamps[index][roomId] ||
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                parseInt(userTimestamps[index][roomId], 10) > lastMsgTimestamp);
 		}
-		const isMembers = await db.isMemberOfSortedSets(
-			uids.map(uid => `uid:${uid}:chat:rooms:unread`),
-			roomId
-		);
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+		const isMembers = yield db.isMemberOfSortedSets(uids.map(uid => `uid:${uid}:chat:rooms:unread`), roomId);
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 		return uids.map((uid, index) => !isMembers[index]);
-	};
-
-	Messaging.markAllRead = async (uid) => {
-		await db.delete(`uid:${uid}:chat:rooms:unread`);
-	};
-
-	Messaging.markUnread = async (uids, roomId) => {
-		const exists = await Messaging.roomExists(roomId);
+	});
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+	Messaging.markAllRead = uid => __awaiter(this, undefined, undefined, function* () { yield db.delete(`uid:${uid}:chat:rooms:unread`); });
+	Messaging.markUnread = (uids, roomId) => __awaiter(this, undefined, undefined, function* () {
+		const exists = yield Messaging.roomExists(roomId);
 		if (!exists) {
 			return;
 		}
 		const keys = uids.map(uid => `uid:${uid}:chat:rooms:unread`);
-		await db.sortedSetsAdd(keys, Date.now(), roomId);
-	};
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+		yield db.sortedSetsAdd(keys, Date.now(), roomId);
+	});
 };
