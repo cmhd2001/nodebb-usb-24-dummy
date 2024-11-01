8fe6ab05673294afb9a73fbf2e96185113e811af
A6575 <astridaalvaradoa@gmail.com>
2024-11-01 14:30:22 -0400

    Modificacion del archivo para que cumpla con las reglas de formato de ESLint

--- public/src/client/groups/list.js
+++ public/src/client/groups/list.js
@@ -12,57 +12,56 @@ define('forum/groups/list', [
 		$('button[data-action="new"]').on('click', function () {
 			// Modificamos el prompt de Bootbox para incluir los nuevos campos
 			const currentYear = new Date().getFullYear();
-            const yearOptions = Array.from({length: 10}, (_, i) => `<option value="${currentYear + i}">${currentYear + i}</option>`).join('');
-			const seccOptions = Array.from({length: 10}, (_, i) => `<option value="${i+1}">${i+1}</option>`).join('');
+			const yearOptions = Array.from({ length: 10 }, (_, i) => `<option value="${currentYear + i}">${currentYear + i}</option>`).join('');
+			const seccOptions = Array.from({ length: 10 }, (_, i) => `<option value="${i + 1}">${i + 1}</option>`).join('');
 
 			bootbox.dialog({
 				title: '[[groups:new-group.group-name]]',
-                message: '<p>[[groups:new-group.course-code]]:</p>'+
-						 '<p><input id="newGroupCode" class="bootbox-input bootbox-input-text form-control" autocomplete="off" type="text" placeholder=[[groups:new-group.course-code]]></p>' +
-                         '<p>[[groups:new-group.course-name]]:</p>'+
-						 '<p><input id="newGroupName" class="bootbox-input bootbox-input-text form-control" autocomplete="off" type="text" placeholder=[[groups:new-group.course-name]]></p>' +
-						 '<p>[[groups:new-group.course-trim]]:</p>'+
-						 '<select id="newGroupTrim" class="form-control">' +
-						 	'<option value=""> —— </option>' +
-							'<option value="[[groups:new-group.course-trim.jm]]">[[groups:new-group.course-trim.jm]]</option>' +
-							'<option value="[[groups:new-group.course-trim.aj]]">[[groups:new-group.course-trim.aj]]</option>' +
-							'<option value="[[groups:new-group.course-trim.sd]]">[[groups:new-group.course-trim.sd]]</option>' +
-							'<option value="[[groups:new-group.course-trim.smmr]]">[[groups:new-group.course-trim.smmr]]</option>' +
-						 '</select></br>' +
-						 '<p>[[groups:new-group.course-year]]:</p>'+
-						 '<select class="form-control" id="newGroupYear">'+
-                         	`${yearOptions}` +
-                         '</select></br>' +
-						 '<p>[[groups:new-group.course-secc]]:</p>'+
-						 '<select class="form-control" id="newGroupSecc">'+
-                         	`${seccOptions}` +
-                         '</select>',
-                buttons: {
-                    cancel: {
-                        label: 'Cancel',
-                        className: 'btn-default',
-                    },
-                    ok: {
-                        label: 'OK',
-                        className: 'btn-primary',
-                        callback: function () {
+				message: '<p>[[groups:new-group.course-code]]:</p>' +
+						'<p><input id="newGroupCode" class="bootbox-input bootbox-input-text form-control" autocomplete="off" type="text" placeholder=[[groups:new-group.course-code]]></p>' +
+                        '<p>[[groups:new-group.course-name]]:</p>' +
+						'<p><input id="newGroupName" class="bootbox-input bootbox-input-text form-control" autocomplete="off" type="text" placeholder=[[groups:new-group.course-name]]></p>' +
+						'<p>[[groups:new-group.course-trim]]:</p>' +
+						'<select id="newGroupTrim" class="form-control">' +
+						'<option value=""> —— </option>' +
+						'<option value="[[groups:new-group.course-trim.jm]]">[[groups:new-group.course-trim.jm]]</option>' +
+						'<option value="[[groups:new-group.course-trim.aj]]">[[groups:new-group.course-trim.aj]]</option>' +
+						'<option value="[[groups:new-group.course-trim.sd]]">[[groups:new-group.course-trim.sd]]</option>' +
+						'<option value="[[groups:new-group.course-trim.smmr]]">[[groups:new-group.course-trim.smmr]]</option>' +
+						'</select></br>' +
+						'<p>[[groups:new-group.course-year]]:</p>' +
+						'<select class="form-control" id="newGroupYear">' +
+                        `${yearOptions}` +
+                        '</select></br>' +
+						'<p>[[groups:new-group.course-secc]]:</p>' +
+						'<select class="form-control" id="newGroupSecc">' +
+                        `${seccOptions}` +
+                        '</select>',
+				buttons: {
+					cancel: {
+						label: 'Cancel',
+						className: 'btn-default',
+					},
+					ok: {
+						label: 'OK',
+						className: 'btn-primary',
+						callback: function () {
 							var code = $('#newGroupCode').val();
-                            var name = $('#newGroupName').val();
+							var name = $('#newGroupName').val();
 							var trimestre = $('#newGroupTrim').val();
 							var year = $('#newGroupYear').val();
 							var seccion = $('#newGroupSecc').val();
-                            if (name && name.length && code && code.length && trimestre && trimestre.length) {
-                                api.post('/groups', {
-                                    name: code + " | " + name + " | " + trimestre + " " + year + " | " + "Prof. " + app.user.username + " | " + "Sec. " + seccion,
-                                }).then((res) => {
-                                    ajaxify.go('groups/' + res.slug);
-                                }).catch(alerts.error);
-                            }
-                        }
-                    }
-                }
-            });
-			createYearPicker($('.bootbox'));
+							if (name && name.length && code && code.length && trimestre && trimestre.length) {
+								api.post('/groups', {
+									name: `${code} | ${name} | ${trimestre} ${year} | Prof. ${app.user.username} | Sec. ${seccion}`,
+								}).then((res) => {
+									ajaxify.go('groups/' + res.slug);
+								}).catch(alerts.error);
+							}
+						},
+					},
+				},
+			});
 		});
 		const params = utils.params();
 		$('#search-sort').val(params.sort || 'alpha');
