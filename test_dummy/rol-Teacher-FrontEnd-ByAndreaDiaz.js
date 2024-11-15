7dd7865aa290410732f1ff6739225a95fe172369
Diaz M Andrea V <106710264+AndreaDiMartin@users.noreply.github.com>
2024-11-15 11:41:34 -0400

    Se mueven los tests de rol-teacher.js a rol-Teacher-FrontEnd-ByAndreaDiaz.js (#85)

--- test_dummy/rol-Teacher-FrontEnd-ByAndreaDiaz.js
+++ test_dummy/rol-Teacher-FrontEnd-ByAndreaDiaz.js
@@ -1,33 +1,59 @@
 'use strict';
 
 const assert = require('assert');
-
 const db = require('./mocks/databasemock');
 const User = require('../src/user');
+const Categories = require('../src/categories');
 const groups = require('../src/groups');
 
 describe('User', () => {
-	// Variables globales
+	let userData;
+	let testUid;
+	let testCid;
 
-	before(async () => {
-		global.config = { userLang: 'en-GB' };
+
+	before(() => {
+		userData = {
+			username: 'John Smith',
+			fullname: 'John Smith McNamara',
+			password: 'swordfish',
+			email: 'john@example.com',
+			callback: undefined,
+		};
 	});
 
-	describe('description general', () => {
-		it('context 1', async () => {
 
+	describe('isProfessor', () => {
+		// Este test verifica que un usuario pueda ser creado con el rol de profesor
+		it('should be able to create an user with teacher role', async () => {
+			testUid = await User.create({ username: 'Profesor', password: 'profesor', isProfessor: 'true' });
+			assert.ok(testUid);
+			const teacher = await groups.isMember(testUid, 'Teachers');
+			assert.strictEqual(teacher, true);
 		});
 
-		it('context 2', async () => {
-
+		// Este test verifica que un usuario pueda ser creado sin el rol de profesor
+		it('should not be in teachers group, if not selected teacher role', async () => {
+			testUid = await User.create({ username: userData.username, password: userData.password });
+			const student = await groups.isMember(testUid, 'Teachers');
+			assert.strictEqual(student, false);
 		});
 
-		it('context 3', async () => {
-
+		// Este test verifica que un usuario creado con el rol de profesor no pueda ser parte del grupo de administradores
+		it('should not be in administrator group, if teacher', async () => {
+			// Se crea un admin primero
+			const admin = await User.create({ username: 'Admin', password: 'admin1235' });
+			testUid = await User.create({ username: 'Profesor', password: 'profesor', isProfessor: 'true' });
+			const teacher = await groups.isMember(testUid, 'administrators');
+			assert.strictEqual(teacher, false);
 		});
 
-		it('context 4', async () => {
-
+		// Este test verifica que un usuario creado con el rol de profesor
+		// no pueda ser parte del grupo de moderadores globales
+		it('should not be in global moderator group, if teacher', async () => {
+			testUid = await User.create({ username: 'Profesor', password: 'profesor', isProfessor: 'true' });
+			const teacher = await groups.isMember(testUid, 'Global Moderators');
+			assert.strictEqual(teacher, false);
 		});
 	});
 });
